8a0ef7efeb1f40381734cf5319e0b780ead78ae3
ArtemS <ArtemS@DESKTOP-TS4RDIK>
2024-01-30 13:02:35 +0300

    Encoding git

--- Program.cs
+++ Program.cs
@@ -9,9 +9,6 @@ namespace ReportCreater
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
